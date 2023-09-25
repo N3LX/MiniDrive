1953bd649e97cb41d2505f9c7c0d25a72f799b3d
Wojciech Hołowiak <w.holowiak@gmail.com>
2023-09-25 20:37:18 +0200

    Initial commit of slightly modified template from Spring initializr

---
+++ mini-drive/src/main/java/com/n3lx/minidrive/MiniDrive.java
@@ -0,0 +1,13 @@
+package com.n3lx.minidrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDrive {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDrive.class, args);
+	}
+
+}
