5c2b26c5847f610320ea62ce5a9b2a7c61a1d89a
Wojciech Hołowiak <w.holowiak@gmail.com>
2023-10-23 21:50:49 +0200

    Created a new series of tests for checking the security of existing endpoints

--- mini-drive/src/test/java/com/n3lx/minidrive/security/SecurityFilterChainTest.java
+++ mini-drive/src/test/java/com/n3lx/minidrive/security/SecurityFilterChainTest.java
@@ -5,8 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SecurityFilterChainTest {
@@ -15,15 +14,103 @@ public class SecurityFilterChainTest {
     private int port;
 
     @Test
-    public void whoAmI_unauthenticated_unauthorizedStatus() {
+    public void upload_unauthenticated_forbiddenStatus() {
+        given()
+                .port(port)
+                .when()
+                .get("/api/storage/upload")
+                .then()
+                .statusCode(403)
+                .body("error", equalTo("Forbidden"))
+                .body("path", equalTo("/api/storage/upload"))
+                .body("status", equalTo(403))
+                .body("timestamp", notNullValue());
+    }
+
+    @Test
+    public void listFiles_unauthenticated_forbiddenStatus() {
+        given()
+                .port(port)
+                .when()
+                .get("/api/storage/listfiles")
+                .then()
+                .statusCode(403)
+                .body("error", equalTo("Forbidden"))
+                .body("path", equalTo("/api/storage/listfiles"))
+                .body("status", equalTo(403))
+                .body("timestamp", notNullValue());
+    }
+
+    @Test
+    public void load_unauthenticated_forbiddenStatus() {
+        given()
+                .port(port)
+                .when()
+                .get("/api/storage/load")
+                .then()
+                .statusCode(403)
+                .body("error", equalTo("Forbidden"))
+                .body("path", equalTo("/api/storage/load"))
+                .body("status", equalTo(403))
+                .body("timestamp", notNullValue());
+    }
+
+    @Test
+    public void delete_unauthenticated_forbiddenStatus() {
+        given()
+                .port(port)
+                .when()
+                .delete("/api/storage/delete")
+                .then()
+                .statusCode(403)
+                .body("error", equalTo("Forbidden"))
+                .body("path", equalTo("/api/storage/delete"))
+                .body("status", equalTo(403))
+                .body("timestamp", notNullValue());
+    }
+
+    @Test
+    public void rename_unauthenticated_forbiddenStatus() {
+        given()
+                .port(port)
+                .when()
+                .patch("/api/storage/rename")
+                .then()
+                .statusCode(403)
+                .body("error", equalTo("Forbidden"))
+                .body("path", equalTo("/api/storage/rename"))
+                .body("status", equalTo(403))
+                .body("timestamp", notNullValue());
+    }
+
+    @Test
+    public void whoami_unauthenticated_notForbiddenStatus() {
         given()
                 .port(port)
                 .when()
                 .get("/api/auth/whoami")
                 .then()
-                .statusCode(401)
-                .body("message", equalTo("Unauthenticated user"))
-                .body("timestamp", notNullValue());
+                .statusCode(not(403));
+    }
+
+    @Test
+    public void register_unauthenticated_notForbiddenStatus() {
+        given()
+                .port(port)
+                .when()
+                .get("/api/auth/register")
+                .then()
+                .statusCode(not(403));
+    }
+
+    @Test
+    public void login_unauthenticated_notForbiddenStatus() {
+        given()
+                .port(port)
+                .when()
+                .get("/api/auth/login")
+                .then()
+                .statusCode(not(403));
     }
 
 }
